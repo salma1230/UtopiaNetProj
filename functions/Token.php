4654a6b6e73504bc6d99d21118c67dbbc3a111dc
salma <salma.mahmood1230@gmail.com>
2021-04-24 23:43:51 +0100

    tidying up code functions

--- functions/Token.php
+++ functions/Token.php
@@ -1,11 +1,27 @@
 <?php
 class Token {
 
+
+/**
+ * Generates a token value
+ *
+ * param: connection to database ''$conn'
+ *
+ * return: token
+ */
 public static function generate(){
 return $_SESSION['token'] = base64_encode(openssl_random_pseudo_bytes(32));
 
 }
 
+
+/**
+ * Checks if token parameter is equal to session token
+ *
+ * param: connection to database ''$conn'
+ *
+ * return: True if token values are equal, false otherwise
+ */
   public static function check($token){
   if(isset($_SESSION['token']) && $token === $_SESSION['token']){
 unset($_SESSION['token']);
